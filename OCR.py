d4f2a5c52562bc8556de31195705daee206c8fab
Dhruv Kumar <77435632+dhruvkk001@users.noreply.github.com>
2023-05-06 11:23:27 +0530

    Add files via upload

---
+++ OCR.py
@@ -0,0 +1,25 @@
+import cv2
+import pytesseract
+import numpy as np 
+from PIL import Image
+
+def ocr_core(img):
+    text = pytesseract.image_to_string(img)
+    return text
+
+img=cv2.imread('img.png')
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+
+def thresholding(image):
+    return cv2.threshold(image,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+img = get_grayscale(img)
+img = thresholding(img)
+img = remove_noise(img)
+
+print(ocr_core(img))
